3bebe8f876eb987a96b7a6aeefb42a81c8920557
Rei  Matsuda <rei.matsuda@imjp.co.jp>
2019-08-30 18:09:36 +0900

    カードめくる機能実装

--- resources/js/modules/CardUI.js
+++ resources/js/modules/CardUI.js
@@ -1,3 +1,54 @@
 export default class {
-  constructor() {}
+  constructor() {
+    this.$card = document.querySelector('.card');
+    this.bind();
+    this.doesCardOpen = false;
+    this.text = 'open';
+  }
+
+  bind() {
+    this.$card.addEventListener('click', () => {
+      this.handleClickedCard();
+    });
+  }
+  handleClickedCard() {
+    velocity(this.$card, 'finish');
+    this.doesCardOpen ? this.cardClose() : this.cardOpen();
+  }
+
+  cardOpen() {
+    velocity(
+      this.$card,
+      { rotateY: 90 },
+      {
+        duration: 200,
+        loop: 1,
+        progress: (elements, complete) => {
+          if (complete === 1 && this.doesCardOpen === false) {
+            this.doesCardOpen = true;
+            this.$card.classList.add('card-front');
+            this.$card.innerText = this.text;
+          }
+        }
+      }
+    );
+  }
+
+  cardClose() {
+    velocity(
+      this.$card,
+      { rotateY: 90 },
+      {
+        duration: 200,
+        loop: 1,
+        progress: (elements, complete) => {
+          if (complete === 1 && this.doesCardOpen === true) {
+            this.doesCardOpen = false;
+            this.$card.classList.remove('card-front');
+            this.$card.innerText = '';
+          }
+        }
+      }
+    );
+  }
 }
