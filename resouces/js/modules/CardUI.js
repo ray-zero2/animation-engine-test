6d153ddd7f46026e67e697592fc2e5e2b16b223f
ray-zero2 <rei2219.matsuda@gmail.com>
2022-01-03 15:50:59 +0900

    open-card animation

--- resouces/js/modules/CardUI.js
+++ resouces/js/modules/CardUI.js
@@ -1,54 +1,75 @@
-import velocity from 'velocity-animate';
+import { gsap } from 'gsap/all';
+
 export default class CardUI {
   constructor() {
-    this.$card = document.querySelector('.js-card');
+    this.cardElement = document.querySelector('.js-card');
     this.isOpened = false;
+
     this.bind();
   }
 
-  bind() {
-    this.$card.addEventListener('click', () => {
-      this.handleClickCard();
-    });
+  async open() {
+    console.log('open:start');
+    await gsap.fromTo(this.cardElement,
+      {
+        rotateY: 0
+      },
+      {
+      rotateY: 90,
+      duration: .2,
+      repeat: 1,
+      yoyo: true,
+      onRepeat: () => {
+        if(!(this.isOpened)) {
+          this.isOpened = true;
+          this.cardElement.classList.add('card-front');
+          this.cardElement.innerText = 'open';
+        }
+      }
+      // todo: updateでprogressを取得する方法の模索
+      // onUpdateParams: [this],
+      // onUpdate(cardUI) {
+      //   console.log(this.progress(), cardUI);
+      //   if(this.progress() === 1 && !(cardUI.isOpened)) {
+      //     cardUI.isOpened = true;
+      //     cardUI.cardElement.classList.add('card-front');
+      //     cardUI.cardElement.innerText = 'open';
+      //   }
+      // }
+    })
+    console.log('open:end');
+  }
+
+  async close() {
+    console.log('close:start');
+    await gsap.fromTo(this.cardElement,
+      {
+        rotateY: 0
+      },{
+      rotateY: 90,
+      duration: .2,
+      repeat: 1,
+      yoyo: true,
+      onRepeat: () => {
+        if(this.isOpened) {
+          this.isOpened = false;
+          this.cardElement.classList.remove('card-front');
+          this.cardElement.innerText = '';
+        }
+      }
+    })
+    console.log('close:end');
   }
 
   handleClickCard() {
-    this.isOpened ? this.close() : this.open();
+    if(this.isOpened) {
+      this.close()
+    } else {
+      this.open();
+    }
   }
 
-  open() {
-    velocity(
-      this.$card,
-      { rotateY: 90 },
-      {
-        duration: 200,
-        loop: 1,
-        progress: (elements, complete) => {
-          if (!this.isOpened && complete === 1) {
-            this.isOpened = true;
-            this.$card.classList.add('card-front');
-            this.$card.innerText = 'open';
-          }
-        }
-      }
-    );
-  }
-
-  close() {
-    velocity(
-      this.$card,
-      { rotateY: 90 },
-      {
-        duration: 200,
-        loop: 1,
-        progress: (elements, complete) => {
-          if (this.isOpened && complete === 1) {
-            this.isOpened = false;
-            this.$card.classList.remove('card-front');
-            this.$card.innerText = '';
-          }
-        }
-      }
-    );
+  bind() {
+    this.cardElement.addEventListener('click', this.handleClickCard.bind(this), { passive: true });
   }
 }
