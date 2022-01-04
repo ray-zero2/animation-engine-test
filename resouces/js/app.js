27cc240ec04a09d67ecf862a01796c17a20e98c2
ray-zero2 <rei2219.matsuda@gmail.com>
2022-01-04 16:58:18 +0900

    create reveal effect

--- resouces/js/app.js
+++ resouces/js/app.js
@@ -1,11 +1,13 @@
 import CardUI from './modules/CardUI';
 import LoopedAnimation from './modules/LoopedAnimation';
-import IntersectionVelocity from './modules/IntersectionVelocity';
+import ShownAnimation from './modules/shownAnimation';
+// import IntersectionVelocity from './modules/IntersectionVelocity';
 class App {
   constructor() {
     new CardUI();
     new LoopedAnimation();
-    new IntersectionVelocity();
+    // new IntersectionVelocity();
+    new ShownAnimation();
   }
 }
 new App();
